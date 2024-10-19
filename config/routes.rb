ee4484faee092bebe797f0a272879e5904b44c81
= <=>
2024-10-19 14:59:16 +0300

    Better login & signup styling and added post functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :posts
   devise_for :users
   # get "pages/home"
   get "pages/about"
@@ -14,6 +15,9 @@ Rails.application.routes.draw do
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
+  authenticated :user do
+      root to: "feed#show", as: :authenticated_user_root
+  end
   # Defines the root path route ("/")
   root "pages#home"
 end
