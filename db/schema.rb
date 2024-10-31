7cba4529219dea81421af3b3435e0b98caf92c2b
mgiannopoulos24 <mariosgian_2002@yahoo.gr>
2024-10-31 23:59:01 +0200

    Update Post Component and Reaction Fixtures to Pass All Tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_30_090107) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_30_183723) do
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
     t.text "body"
@@ -64,6 +64,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_10_30_090107) do
     t.integer "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "body"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
