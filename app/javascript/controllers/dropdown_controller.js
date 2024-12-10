6b104306eeead001050835f3001968c60f922c91
= <=>
2024-12-10 21:58:17 +0200

    Fixed all browser errors and warnings

--- app/javascript/controllers/dropdown_controller.js
+++ app/javascript/controllers/dropdown_controller.js
@@ -13,19 +13,29 @@ export default class extends Controller {
     if (!this.menuTarget.classList.contains("hidden")) {
       this.menuTarget.classList.add("hidden");
     }
-    if (!this.languageMenuTarget.classList.contains("hidden")) {
+    if (this.hasLanguageMenuTarget && this.languageMenuTarget && !this.languageMenuTarget.classList.contains("hidden")) {
       this.languageMenuTarget.classList.add("hidden");
     }
   }
 
   toggleLanguage(event) {
     event.stopPropagation();
-    this.languageMenuTarget.classList.toggle("hidden");
+    if (this.hasLanguageMenuTarget && this.languageMenuTarget) {
+      this.languageMenuTarget.classList.toggle("hidden");
+    }
   }
 
   closeUnlessDropdown(event) {
     if (!this.element.contains(event.target)) {
       this.close();
     }
+  }
+
+  get hasLanguageMenuTarget() {
+    return this.targets.find("languageMenu") !== undefined;
+  }
+
+  get languageMenuTarget() {
+    return this.targets.find("languageMenu");
   }
 }
