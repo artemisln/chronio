af159e9b7edce5cd46bbc4e8963b9f1a46dc8cbe
mgiannopoulos24 <mariosgian_2002@yahoo.gr>
2025-07-26 00:08:56 +0300

    Change font and cleanup

--- app/javascript/controllers/toggle_search_controller.js
+++
@@ -1,9 +0,0 @@
-import { Controller } from "@hotwired/stimulus";
-
-export default class extends Controller {
-  static targets = ["searchContainer"];
-
-  toggle() {
-    this.searchContainerTarget.classList.toggle("expanded");
-  }
-} 
